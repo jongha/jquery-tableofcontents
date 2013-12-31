9322dca4d39162be5cfd647ad14502100ba8a230
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2013-12-31 12:53:55 +0900

    Build the test case.

--- src/jquery.tableofcontents.js
+++ src/jquery.tableofcontents.js
@@ -1,54 +1,65 @@
 (function ($) {
     $.fn.tableofcontents = function(options) {
         var settings = $.extend({
-            id: 'container' // target element id
+            id: "container", // target element id
+            css: {
+                "padding": "3px", // padding size
+                "border": "solid 1px #CCC", // border style
+                "width": null // width
+            }
         }, options);
 
         options = $.extend(settings, options);
-        var target = $(options.id).append("<ul></ul>");
-        var prev_level = 0;
-
+        
+        var target = $(options.id)
+            .append($("<div></div>").html("Contents").css({ "font-weight": "bold" }))
+            .append("<ul></ul>")
+            .css(options.css);
+            
+        var prevLevel = 0;
         var getLevel = function(tagname) {
             switch(tagname) {
-                case 'H1': return 1;
-                case 'H2': return 2;
-                case 'H3': return 3;
-                case 'H4': return 4;
-                case 'H5': return 5;
-                case 'H6': return 6;
+                case "H1": return 1;
+                case "H2": return 2;
+                case "H3": return 3;
+                case "H4": return 4;
+                case "H5": return 5;
+                case "H6": return 6;
             }
 
             return 0;
         };
 
         var getUniqId = function() {
-            return "__tocid-" + Math.floor(Math.random() * 100000);
+            return "__toc_id:" + Math.floor(Math.random() * 100000);
         };
 
-        this.find('h1, h2, h3, h4, h5, h6').each(function() {
-            var current_level = getLevel($(this)[0].tagName);
-            if(current_level > prev_level) {
-                var tmp = $("<ul></ul>").data("level", current_level);
+        this.find("h1, h2, h3, h4, h5, h6").each(function() {
+            var that = $(this);
+            var currentLevel = getLevel(that[0].tagName);
+            if(currentLevel > prevLevel) {
+                var tmp = $("<ul></ul>").data("level", currentLevel);
                 target = target.append(tmp);
                 target = tmp;
+                
             }else {
-                while(target.parent().length && current_level <= target.parent().data("level")) {
+                while(target.parent().length && currentLevel <= target.parent().data("level")) {
                     target = target.parent();
                 }
             }
 
-            var txt = $(this).html();
-            var txtId = $(this).attr("id");
+            var txt = that.html();
+            var txtId = that.attr("id");
             if(!!!txtId) {
                 txtId = getUniqId();
-                $(this).attr({ "id": txtId });
+                that.attr({ "id": txtId });
             }
 
             var alink = $("<a></a>").text(txt).attr({ "href": "#" + txtId });
-
             target.append($("<li></li>").append(alink));
-            prev_level = current_level;
+            prevLevel = currentLevel;
         });
+        
         return this;
     };
 }(jQuery));
--- test/jquery.tableofcontents_test.js
+++ test/jquery.tableofcontents_test.js
@@ -1,6 +1,9 @@
 ﻿(function($) {
-    test('Test one', function() {
-        equal($(1), [1]);
+    test("Test1", function() {
+        $("#test1").tableofcontents({ id: "#tableofcontents1" });
+        equal($("#tableofcontents1").find("ul").length, 3);
+        equal($("#tableofcontents1").find("li").length, 3);
+        equal($("#tableofcontents1").find("ul, li").length, 6);
     });
 
 }(jQuery));
